6644e0ea1907074dc69173c73c9d479d362e4969
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-03-01 14:15:44 +0500

    fix operations.py

--- units/operations.py
+++ units/operations.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_operations():
-    with open('../operations.json', 'rt', encoding='utf-8') as file:
+    with open('operations.json', 'rt', encoding='utf-8') as file:
         operations_list = json.loads(file.read())
     return operations_list
 
